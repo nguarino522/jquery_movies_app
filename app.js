ad53da199ac68f420221062a370fe16a360f7cfb
nguarino522 <sub4miler5@gmail.com>
2022-09-06 22:48:02 -0400

    started sorting implementation

--- app.js
+++ app.js
@@ -17,6 +17,36 @@ $(document).ready(function(){
         });
     })
 
+    $("i").on("click", function(e){
+        console.log(e);
+        sortTable();
+    });
+
+    function sortTable(){
+        var rows = $('#mytable tbody  tr').get();
+      
+        rows.sort(function(a, b) {
+      
+        var A = $(a).children('td').eq(1).text().toUpperCase();
+        var B = $(b).children('td').eq(1).text().toUpperCase();
+      
+        if(A < B) {
+          return -1;
+        }
+      
+        if(A > B) {
+          return 1;
+        }
+      
+        return 0;
+      
+        });
+      
+        $.each(rows, function(index, row) {
+          $('#mytable').children('tbody').append(row);
+        });
+      }
+
 
 
 });
