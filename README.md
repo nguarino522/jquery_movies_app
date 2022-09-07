fe971dc0e8a09e035abc7126e826a63b409a04bd
Nicholas Guarino <nguarino@datto.lan>
2022-09-07 17:27:32 -0400

    done complete

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # A Simple Movies App to Rate Movies from 1 to 10 using Jquery
 
 Github Pages: https://nguarino522.github.io/jquery_movies_app/
-
-Background Image:
-https://www.wallpaperflare.com/movies-film-reel-technology-projector-8mm-wallpaper-utdnn/download/1920x1080
--- app.js
+++ app.js
@@ -1,52 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("form").on("submit", function(e){
-        e.preventDefault();
-        let movieTitle = $("#movietitle").val();
-        let movieRating = $("#movierating").val();
-        if(movieTitle.length < 2 && movieRating < 10 || movieTitle.length > 10 && movieRating < 0){
-            alert("Please select a title with at least 2 characters and rating between 1 and 10!");
-            return;
-        }
-        
-        $("tbody").append($(`<tr><td>${movieTitle}</td><td>${movieRating}</td><td><a class="waves-effect waves-light btn-small removebtn" id="removebtn">Remove</a></td></tr>`));
+  // create remove button listener for prepopulated ratings
+  $(".removebtn").on("click", function (e) {
+    $(this).closest("tr").remove();
+  });
 
-        $(".removebtn").on("click", function(e){
-            console.log(e);
-            $(this).closest("tr").remove();
-        });
-    })
+  // listener for submit of form to add a rating
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    let movieTitle = $("#movietitle").val();
+    let movieRating = $("#movierating").val();
+    if (movieTitle.length < 2 && movieRating < 10 || movieTitle.length > 10 && movieRating < 0) {
+      alert("Please select a title with at least 2 characters and rating between 1 and 10!");
+      return;
+    }
+    $("tbody").append($(`<tr><td>${movieTitle}</td><td>${movieRating}</td><td><a class="waves-effect waves-light btn-small removebtn" id="removebtn">Remove</a></td></tr>`));
 
-    $("i").on("click", function(e){
-        console.log(e);
-        sortTable();
+    // create event listener for remove button in table
+    $(".removebtn").on("click", function (e) {
+      $(this).closest("tr").remove();
     });
-
-    function sortTable(){
-        var rows = $('#mytable tbody  tr').get();
-      
-        rows.sort(function(a, b) {
-      
-        var A = $(a).children('td').eq(1).text().toUpperCase();
-        var B = $(b).children('td').eq(1).text().toUpperCase();
-      
-        if(A < B) {
-          return -1;
-        }
-      
-        if(A > B) {
-          return 1;
-        }
-      
-        return 0;
-      
-        });
-      
-        $.each(rows, function(index, row) {
-          $('#mytable').children('tbody').append(row);
-        });
-      }
+  })
 
 
+  // listen for click on table heading, code snippet grabbed from here for an easy workable solution: https://stackoverflow.com/a/19947532
+  $('th').click(function () {
+    console.log($(this));
+    let table = $(this).parents('table').eq(0);
+    let rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+    this.asc = !this.asc;
+    if (!this.asc) { rows = rows.reverse() };
+    for (let i = 0; i < rows.length; i++) { table.append(rows[i]) };
+  })
+
+  function comparer(index) {
+    return function (a, b) {
+      let valA = getCellValue(a, index), valB = getCellValue(b, index);
+      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
+    }
+  }
+
+  function getCellValue(row, index) { return $(row).children('td').eq(index).text() };
 
 });
